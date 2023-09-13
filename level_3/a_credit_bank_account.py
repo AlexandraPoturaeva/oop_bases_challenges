2de5f35bb8578d8a994be0c667e34be625f99b49
AlexandraPoturaeva <av-poturaeva@yandex.ru>
2023-09-13 19:46:50 +0300

    level 3, ex. a

--- level_3/a_credit_bank_account.py
+++ level_3/a_credit_bank_account.py
@@ -8,24 +8,41 @@
     4. Создать экземпляр класс CreditAccount и вызвать у него каждый из возможных методов.
 """
 
-# код писать тут
 
-
-class CreditAccount:
-    def __init__(self, owner_full_name: str, balance: float):
+class BankAccount:
+    def __init__(self, owner_full_name: str, balance: float) -> None:
         self.owner_full_name = owner_full_name
         self.balance = balance
 
-    def increase_balance(self, amount: float):
+    def increase_balance(self, amount: float) -> float:
         self.balance += amount
+        return self.balance
 
-    def decrease_balance(self, amount: float):
+    def decrease_balance(self, amount: float) -> float:
         self.balance -= amount
+        return self.balance
 
-    def is_eligible_for_credit(self):
+
+class CreditAccount(BankAccount):
+    def is_eligible_for_credit(self) -> bool:
         return self.balance > 1000
 
 
 if __name__ == '__main__':
-    pass  # код писать тут
+    bank_account = BankAccount(owner_full_name='IVAN IVANOV', balance=10000)
+    print(
+        f'Движение по банковскому счёту: '
+        f'\n1.{bank_account.balance} '
+        f'\n2.{bank_account.increase_balance(100)}'
+        f'\n3.{bank_account.decrease_balance(200)}')
 
+    credit_account = CreditAccount(owner_full_name='IVAN IVANOV', balance=10000)
+    print(f'\nДвижение по кредитному счёту: '
+          f'\n1.{credit_account.balance} '
+          f'\n2.{credit_account.increase_balance(500)}'
+          f'\n3.{credit_account.decrease_balance(2000)}')
+
+    print(
+        'Право на получение кредита: ' +
+        ('есть' if credit_account.is_eligible_for_credit() else 'нет')
+    )
